9ba76d46d76d7d3dfd416f2e330b5bd4fff72fbc
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-09-29 17:28:43 -0300

    Footer adapted to react logic

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,37 +1,48 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import Logo from "../../assets/Logo.png";
 
+
+
 export default function AppFooter() {
-    return (
+  return (
+
+   // bg-primary es un color de fondo, text-light color de texto, py-4 para dar un padding de 4
+    <footer className="bg-primary text-light py-4"> 
+      
+      <Container>
         
-        <footer>
+        <Row className="gy-4"> 
 
-            <div className="Footer">
-                <div className="Footer-Brand">
-                    <img src={Logo} alt="Logo Pokémon" />
-                    <p>Centro Pokémon - ¡Atrapálos a todos!</p>
-                </div>
+          <Col lg={3} md={6} xs={12} className="text-center text-md-start">
+            <img src={Logo} alt="Logo Pokémon" style={{ width: '80px', marginBottom: '0.5rem' }} />
+            <p>Centro Pokémon - ¡Atrápalos a todos!</p>
+          </Col>
 
-                <div className="Footer-Contacto">
-                    <h4>Servicio Técnico</h4>
-                    <p>Email: serviciotecnico@centropokemon.cl</p>
-                    <p>Tel: +569 12345678</p>
-                    <a href="#">
-                        <i className="fab fa-whatsapp"></i>
-                    </a>
-                </div>
+          <Col lg={3} md={6} xs={12} className="text-center text-md-start">
+            <h4>Servicio Técnico</h4>
+            <p className="mb-1">Email: serviciotecnico@centropokemon.cl</p>
+            <p className="mb-1">Tel: +569 12345678</p>
+            <a href="#" className="text-light fs-4">
+              <i className="fab fa-whatsapp"></i>
+            </a>
+          </Col>
+          
+          <Col lg={3} md={6} xs={12} className="text-center text-md-start">
+            <h4>Síguenos</h4>
+            <a href="#" className="text-light fs-4 me-3"><i className="fab fa-facebook-f"></i></a>
+            <a href="#" className="text-light fs-4 me-3"><i className="fab fa-twitter"></i></a>
+            <a href="#" className="text-light fs-4 me-3"><i className="fab fa-instagram"></i></a>
+            <a href="#" className="text-light fs-4"><i className="fab fa-youtube"></i></a>
+          </Col>
 
-                <div className="Footer-Social">
-                    <h4>Síguenos</h4>
-                    <a href="#"><i className="fab fa-facebook-f"></i></a>
-                    <a href="#"><i className="fab fa-twitter"></i></a>
-                    <a href="#"><i className="fab fa-instagram"></i></a>
-                    <a href="#"><i className="fab fa-youtube"></i></a>
-                </div>
+        
+          <Col lg={3} md={6} xs={12} className="text-center text-md-start">
+         
+             <p className="mt-4">&copy; 2025 Tienda Pokémon. Todos los derechos reservados.</p>
+          </Col>
 
-                <div className="Footer-Fondo">
-                    <p>&copy; 2025 Tienda Pokémon. Todos los derechos reservados.</p>
-                </div>
-            </div>
-        </footer>
-    );
+        </Row>
+      </Container>
+    </footer>
+  );
 }
