4e874bc7fae550647521917a5690e82939c95b65
nerxzel <felipe.e.becerra@gmail.com>
2025-10-01 20:53:15 -0300

    se intentó hacer la seccion de noticias

---
+++ src/utils/mockNoticias.js
@@ -0,0 +1,15 @@
+import megaDragonite from '../assets/mega-dragonite.png';
+import mecanica from '../assets/mecanica.jpg';
+import noticiatcg from '../assets/noticia-tcg.jpg';
+import pokemonGo from '../assets/pokemon-go-2025.png';
+import tamagochi from '../assets/tamagochi.jpg';
+import megasNuevos from '../assets/MegasNuevos';
+
+export const noticiasMock = [
+    {id: 1, imagen: megaDragonite , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
+    {id: 2, imagen: mecanica , titulo: "El millonario mundo de las cartas Pokemon", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
+    {id: 3, imagen: noticiatcg , titulo: "El millonario mundo de las cartas Pokemon", descripcion: "En Chile hay unidades que superan los $1,5 millones y hay carpetas cuyo valor alcanzan los $12 millones."},
+    {id: 4, imagen: pokemonGo , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
+    {id: 5, imagen: tamagochi , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
+    {id: 6, imagen: megasNuevos , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
+]
