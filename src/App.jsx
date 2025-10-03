81b6f61f268449d1b9dd774c9feb82e66b5811de
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-10-03 09:38:00 -0300

    news section finished

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
       <div className="d-flex flex-column min-vh-100">
         <AppNavbar />
           <main className="flex-grow-1 py-4">
-            <Container className="bg-white rounded p-4 shadow-sm h-100">
+            <Container className="bg-white rounded-4 p-4 shadow-sm h-100">
               <AppRoutes />
             </Container>
           </main>
--- src/utils/mockNoticias.js
+++ src/utils/mockNoticias.js
@@ -3,13 +3,25 @@ import mecanica from '../assets/mecanica.jpg';
 import noticiatcg from '../assets/noticia-tcg.jpg';
 import pokemonGo from '../assets/pokemon-go-2025.png';
 import tamagochi from '../assets/tamagochi.jpg';
-import megasNuevos from '../assets/MegasNuevos';
+import MegasNuevos from '../assets/MegasNuevos.jpg';
+import MegaMalamar from '../assets/MegaMalamar.jpg';
+import Pokopia from '../assets/Pokopia.png';
+import MegaVictreebel from '../assets/MegaVictreebel.jpg';
+import Trailer from '../assets/Trailer.jpg';
+import TerminalMontage from '../assets/maxresdefault.jpg';
+import MegaDimension from '../assets/MegaDimension.jpg';
 
 export const noticiasMock = [
-    {id: 1, imagen: megaDragonite , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
-    {id: 2, imagen: mecanica , titulo: "El millonario mundo de las cartas Pokemon", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
-    {id: 3, imagen: noticiatcg , titulo: "El millonario mundo de las cartas Pokemon", descripcion: "En Chile hay unidades que superan los $1,5 millones y hay carpetas cuyo valor alcanzan los $12 millones."},
-    {id: 4, imagen: pokemonGo , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
-    {id: 5, imagen: tamagochi , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
-    {id: 6, imagen: megasNuevos , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo"},
+    {id: 1, imagen: megaDragonite , titulo: "Se anuncia el primer nuevo Mega", descripcion: "Horrible dragón amarillo destruye los sueños de los niños alrededor del mundo", url: 'https://www.youtube.com/watch?v=fWDx1omjIwM'},
+    {id: 2, imagen: mecanica , titulo: "Nintendo patenta una mecánica que puede afectar a muchos juegos", descripcion: "La batalla entre Nintendo y Palworld parece seguir adelante", url: 'https://nintenduo.com/nueva-patente-nintendo-mecanica-invocacion-combate'},
+    {id: 3, imagen: noticiatcg , titulo: "El millonario mundo de las cartas Pokemon", descripcion: "En Chile hay unidades que superan los $1,5 millones y hay carpetas cuyo valor alcanzan los $12 millones.", url: 'https://www.emol.com/noticias/Economia/2025/08/13/1174905/pokemon-cartas-negocio-millonario.html'},
+    {id: 4, imagen: pokemonGo , titulo: "Niantic vende Pokemon GO", descripcion: "Resulta que Pokémon GO tendrá otro desarrollador, ya que Niantic ha anunciado que será vendido a Scopely", url: 'https://www.movilzona.es/noticias/juegos/niantic-vende-pokemon-go'},
+    {id: 5, imagen: tamagochi , titulo: "Pokemon anuncia un nuevo ‘Tamagotchi’ oficial con más de 150 criaturas distintasa", descripcion: "Poké-Nade es el nuevo juguete interactivo de Pokemon al estilo Tamagotchi", url: 'https://as.com/meristation/noticias/pokemon-anuncia-un-nuevo-tamagotchi-oficial-con-mas-de-150-criaturas-distintas-todo-un-sueno-hecho-realidad-n'},
+    {id: 6, imagen: MegasNuevos , titulo: "Mega evoluciones para los iniciales de Kalos", descripcion: "Geniales Megas restauran los sueños de los niños alrededor del mundo", url: 'https://www.youtube.com/watch?v=0-LSVywqUek'},
+    {id: 7, imagen: MegaMalamar , titulo: "¡Conoce a Mega Malamar y su brillo deslumbrante!", descripcion: "La Mega Evolución ha hinchado su cerebro y potenciado sus poderes psiquicos", url: 'https://www.youtube.com/watch?v=0-LSVywqUek'},
+    {id: 8, imagen: Pokopia , titulo: "¿Animal Crossing x Minecraft x Pokemon?", descripcion: "Conoce más detalles del nuevo juego de Pokemon el cuál protagonizará un Ditto", url: 'https://www.pokemon.com/el/noticias-pokemon/pokemon-pokopia-llega-en-2026'},
+    {id: 9, imagen: MegaVictreebel , titulo: "Mega Victreebel llega lleno de ácido", descripcion: "El olor del ácido es tan dulce que maréa a quien lo huele", url: 'https://www.youtube.com/watch?v=Dq3SPAGCbBo'},
+    {id: 10, imagen: Trailer , titulo: "Nuevo trailer Pokemon AZ", descripcion: "Resumen de lo que sabemos del juego hasta el momento", url: 'https://www.youtube.com/watch?v=AqTHAdEIDek'},
+    {id: 11, imagen: TerminalMontage , titulo: "¿Quién ganaría un Battle Royale entre los Pokemon Paradox?", descripcion: "Terminal Montage hace cómicas animaciones sobre el mundo Pokemon", url: 'https://www.youtube.com/watch?v=-OUVRcJMcfQ'},
+    {id: 12, imagen: MegaDimension , titulo: "Se anuncia primer DLC de Pokemon AZ", descripcion: "¿Qué opinas de que se anuncie un nuevo DLC antes del lanzamiento del juego?", url: 'https://www.youtube.com/watch?v=ExUYVdlA6eI'},
 ]
