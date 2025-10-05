c872fadad5ab5c66d924368a20251b0b44ac62f0
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-10-05 01:43:46 -0300

    change in router logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import AppNavbar from "./components/layout/Navbar";
 import AppFooter from "./components/layout/Footer";
-import AppRoutes from "./routes/AppRoutes";
 import './style/theme.css';
 import { Container } from "react-bootstrap";
 
-export default function App() {
+export default function App({children}) {
   return(
     <>
       <div className="d-flex flex-column min-vh-100">
         <AppNavbar />
           <main className="flex-grow-1 py-4">
             <Container className="bg-white rounded-4 p-4 shadow-sm h-100">
-              <AppRoutes />
+              {children}
             </Container>
           </main>
         <AppFooter />
