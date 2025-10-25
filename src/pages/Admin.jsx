7a0d5e875e582baf6632c1853b98b8cd7d522b78
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-10-25 04:38:42 -0300

    admin under construction

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,14 @@
+import Lapras from '../assets/lapras_construccion.png'
+
 function Admin() {
   return (
-    <div>
-      <h1>Admin</h1>
-      <p>Página en construcción...</p>
+    <div className='d-flex flex-column align-items-center'> 
+      <h2>En construcción</h2>
+      <img src={Lapras} 
+           alt="Lapras" 
+           width="500" 
+           height="600" />
     </div>
   );
 }
-
 export default Admin;
