9c96ec9bbb86ebda7484dad6795ccb365eb8e3a1
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-11-19 23:00:44 -0300

    configuración inicial de axios para JWT y peticiones al backend

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+    'Content-Type': 'application/json',
+    'x-tenant-id': import.meta.env.VITE_TENANT_ID, 
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const storedUser = localStorage.getItem('currentUser');
+        if (storedUser) {
+            const user = JSON.parse(storedUser);
+        if (user?.token) {
+            config.headers.Authorization = `Bearer ${user.token}`;
+        }
+        }
+            return config;
+    },
+    (error) => Promise.reject(error)
+    );
+
+export default api;
